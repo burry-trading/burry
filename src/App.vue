59d2b03a96700c888d987f941d391f590c8a75a3
Waldrey <waldrey22@gmail.com>
2022-07-30 21:50:32 -0300

    feat: initial vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/burry.png">
+  <h1>Burry Trading</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'DM Sans', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
