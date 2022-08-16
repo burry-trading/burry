953c3cab3c1980bf0e453a91255bc9446d2ec17f
Waldrey <waldrey22@gmail.com>
2022-08-16 08:23:57 -0300

    feat: token expired in axios return user to login

--- src/services/burry.service.js
+++ src/services/burry.service.js
@@ -4,4 +4,17 @@ const instance = axios.create({
   baseURL: process.env.VUE_APP_SERVICE
 });
 
+instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('tokenUser')}`;
+instance.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (error.response.status === 401) {
+    localStorage.removeItem('userData');
+    localStorage.removeItem('tokenUser');
+    window.location = '/';
+  }
+
+  return Promise.reject(error);
+});
+
 export default instance;
